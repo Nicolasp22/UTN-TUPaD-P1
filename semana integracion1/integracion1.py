3f46a7b1311d2dfc597eb976223aa2e2acd721bd
Nicolas Pagola <nicolaspagola@outlook.com>
2025-04-15 20:50:26 -0300

    Avances

--- semana integracion1/integracion1.py
+++ semana integracion1/integracion1.py
@@ -1,8 +1,23 @@
-numero=int(input("Ingrese un numero decimal para convertir a binario: "))
+numero=int(input("Ingrese un numero decimal positivo para convertirlo: "))
 resultado=[]
-while numero>0:
-    resultado.append(str(numero%2))
-    numero=numero//2
+opciones = str(input("Indique a que sistema quiere convertir el numero ingresado (B, O, H): "))
+opciones = opciones.upper().strip()
 
-final=''.join(reversed(resultado))
-print(final)
+if opciones == "B":
+    while numero>0:
+        resultado.append(str(numero%2))
+        numero=numero//2
+        final=''.join(reversed(resultado))
+    print(f"El numero decimal {numero} equivale a {final} en el sistema binario.")
+elif opciones == "O":
+    while numero>0:
+        resultado.append(str(numero%8))
+        numero=numero//8
+        final=''.join(reversed(resultado))
+    print(f"El numero decimal {numero} equivale a {final} en el sistema octal.")
+elif opciones == "H":
+    final = hex(numero)[2:]
+    print(f"El numero decimal {numero} equivale a {final} en el sistema hexadecimal.")
+else:
+    print("Opcion no válida")
+
